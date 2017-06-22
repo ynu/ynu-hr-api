a29dceb73af27694ea3f5ed5736e8d2fc3bf9427
liudonghua123 <liudonghua123@gmail.com>
2017-06-22 14:30:39 +0800

    add docker releated stuff

---
+++ server/datasources.production.js
@@ -0,0 +1,10 @@
+module.exports = {
+  mysql_hr: {
+    connector: 'mysql',
+    host: process.env.MYSQL_HOST || 'localhost',
+    port: process.env.MYSQL_PORT || 3306,
+    user: process.env.MYSQL_USER || 'root',
+    password: process.env.MYSQL_PASSWORD || '',
+    database: process.env.MYSQL_DATABASE || 'test',
+  }
+};
